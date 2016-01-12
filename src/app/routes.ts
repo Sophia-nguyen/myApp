9515bcf88dd8f5412770fbff53eca7b730581a53
thuytn <Sophia-nguyen@users.noreply.github.com>
2016-01-12 23:23:54 +0700

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,6 @@
 import {RouteDefinition} from 'angular2/router';
-import {HomeComponent} from './components/home';
-import {LoginComponent} from './components/login';
+import {HomeComponent} from './components/home/home';
+import {LoginComponent} from './components/login/login';
 
 export var APP_ROUTES: RouteDefinition[] = [
     { path: '/home', name: 'Home', component: HomeComponent, useAsDefault: true },
