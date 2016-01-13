be859e2db203ba1d21b0f0d998d386c85cee602a
thuytn <Sophia-nguyen@users.noreply.github.com>
2016-01-13 18:23:05 +0700

    Update gulp.config.js

--- gulp.config.js
+++ gulp.config.js
@@ -52,7 +52,7 @@ module.exports = function () {
             port: 8080,
             host: "127.0.0.1",
             open: '/',
-            root: 'dist/',
+            root: '.',
             file: "index.html",
             wait: 1000
         },
